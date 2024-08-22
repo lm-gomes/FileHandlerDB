bec3f8a980e7dfd67c66e40ca98079fba4876ece
Lucas Moreira Gomes <105596835+lm-gomes@users.noreply.github.com>
2024-08-22 10:24:07 -0300

    help menu added
    a menu that contains some tips

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
 
 void menu();
 void cadastrarAluno();
@@ -16,39 +17,48 @@ int idAluno = 0;
 int main(){
     file = fopen("text.txt", "r");
     if(file == NULL){
-        printf("The file was not found in the directory.\n\n");
-        printf("Creating a new file...");
+        char *temp = malloc(sizeof(char));
+        printf("O arquivo nao foi encontrado no diretorio.\n\n");
+        printf("Criando novo arquivo...");
         file = fopen("text.txt", "a");
         fprintf(file, "[ARQUIVO] - 0 alunos cadastrados.\n");
-        printf("File created succesfully!\n\n");
-
+        printf("Arquivo criado com sucesso!\n\nDigite qualquer tecla para prosseguir.\n>> ");
+        scanf("%d", temp);
+        free(temp);
     }
     fclose(file);
     menu();
 
     if(fclose(file) != EOF){
-        printf("Couldn't close the file.\n\n");
+        printf("Nao foi possivel fechar o arquivo.\n\n");
         return 1;
     }
     return 0;
 }
 
 void menu(){
+    clearTerminal();
     alunosCD = alunosCadastrados(alunosCD);
     printf("\n - Alunos cadastrados: %d - \n", alunosCD);
     printf("\n- - - CONSULTA - - - \n");
-    printf("[1] Cadastrar aluno\n[2] Consultar alunos\n[3] Sair\n>> ");
+    printf("[1] Cadastrar aluno\n[2] Consultar alunos\n[3] Ajuda\n[4] Sair\n>> ");
     scanf("%d", &userInput);
     switch(userInput){
         case 1:{
+            clearTerminal();
             cadastrarAluno();
             break;
         }
         case 2:{
+            clearTerminal();
             consultarAluno();
             break;
         }
         case 3:{
+            clearTerminal();
+            ajudaMenu();
+        }
+        case 4:{
 
             break;
         }
@@ -70,15 +80,16 @@ void cadastrarAluno(){
         scanf("%[^\n]", name);
         printf("Digite a matricula: ");
         scanf("%d", &matricula);
-        //Necessário adicionar um contador de alunos cadastrados após essa função.
-        fprintf(file, "(%d) Aluno: %s - Matricula: %d\n", alunosCD + 1, name, matricula);
-        alunosCD++;
-        fclose(file);
-        atualizarArquivo(1);
-
-
-        printf("Aluno cadastrado!\nGostaria de continuar cadastrando?\n>> ");
+        printf("Cadastrar aluno %s de matricula %d?\n>> ", name, matricula);
         scanf("%d", &userInput);
+        if(userInput != 0){
+            fprintf(file, "(%d) Aluno: %s - Matricula: %d\n", alunosCD + 1, name, matricula);
+            alunosCD++;
+            fclose(file);
+            atualizarArquivo(1);
+            printf("Aluno cadastrado!\nGostaria de continuar cadastrando?\n>> ");
+            scanf("%d", &userInput);
+        }
     }
 
     menu();
@@ -86,7 +97,6 @@ void cadastrarAluno(){
 }
 
 void consultarAluno(){
-
     char userConfirm;
     // Stream no topo do arquivo
     file = fopen("text.txt", "r");
@@ -115,9 +125,10 @@ int alunosCadastrados(int alunosCD){
 }
 
 void atualizarArquivo(int op){
-    FILE *fileTemp = fopen("temp.txt", "w");
+
     switch(op){
         case 1:{
+            FILE *fileTemp = fopen("temp.txt", "w");
             int nCount = 0;
             fprintf(fileTemp, "[ARQUIVO] - %d alunos cadastrados.\n", alunosCD);
             file = fopen("text.txt", "r");
@@ -142,12 +153,12 @@ void atualizarArquivo(int op){
             }
             remove("text.txt");
             rename("temp.txt", "text.txt");
-
             break;
         };
 
         case 2:{
             if(alunosCD != 0){
+                FILE *fileTemp = fopen("temp.txt", "w");
                 int userID = 0, nCount = 0, idCount = 0;
                 printf("Digite o ID do aluno que deseja REMOVER: ");
                 scanf("%d", &userID);
@@ -186,14 +197,17 @@ void atualizarArquivo(int op){
                     }
                     remove("text.txt");
                     rename("temp.txt", "text.txt");
+
                 }
                 else{
                     printf("Nao existe aluno com esse ID!\n");
+                    Sleep(2000);
                 }
             }
 
             else{
                 printf("Nao existem alunos para serem removidos!\n");
+                Sleep(2000);
 
             }
                 break;
@@ -203,8 +217,23 @@ void atualizarArquivo(int op){
 
 }
 
+void ajudaMenu(){
+    int userAjuda = 0;
+    printf("Bem-vindo ao programa de cadastro e consulta de alunos!\nAtraves deste programa e possivel cadastrar alunos e remove-los atraves de seus IDs.\n\nImportante:\n-> Utilize 1 para confirmar operacoes e 0 para cancelar.\n\n-> Os arquivos devem ser manipulados apenas atraves do programa.\n\n-> Os dados ficarao salvos apos o programa ser finalizado.\n\n-> O arquivo 'text.txt' deve ser mantido com o mesmo nome e na mesma pasta do projeto para que o programa funcione corretamente.\n\n");
+    printf("[1]Voltar para menu\n>> ");
+    scanf("%d", &userAjuda);
+    if(userAjuda == 1){
+        menu();
+    }
+
+}
+
 
 void cleanBuffer(){
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void clearTerminal(){
+    system("cls");
 }
